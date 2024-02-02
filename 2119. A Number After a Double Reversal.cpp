724c4a13c9c0e4c9862dd504a49dda1825fcad9b
Shozab Mehdi <shozabmehdi89@gmail.com>
2024-02-02 14:30:09 +0500

    2119. A Number After a Double Reversal.cpp

---
+++ 2119. A Number After a Double Reversal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        int rev1=0, rev2=0, original_num=num;
+        while (num!=0){
+            int temp = num%10;
+            rev1 = (rev1*10) + temp;
+            num=num/10;
+        }
+        while (rev1!=0){
+            int temp = rev1%10;
+            rev2 = (rev2*10) + temp;
+            rev1=rev1/10;
+        }
+    if (rev2==original_num){
+        return (true);
+    }
+    else{
+        return (false);
+    }
+    }
+};
