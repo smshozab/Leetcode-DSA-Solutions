e43efb46eb31f2102d1bb170bf200c7f5ca9c2b6
Shozab Mehdi <shozabmehdi89@gmail.com>
2024-02-06 20:48:00 +0500

    ReverseInteger.cpp Tue Feb 06 2024 20:48:00 GMT+0500 (Pakistan Standard Time)

---
+++ ReverseInteger.cpp
@@ -0,0 +1,13 @@
+class Solution {public:
+   int reverse(int x) {
+       int rev=0;
+       while (x!=0){
+           int temp = x%10;
+           if (rev<INT_MIN/10 ||                 rev>INT_MAX/10){
+           return 0;
+       }
+           rev = (rev*10) + temp;
+           x=x/10;
+       }
+       return rev;
+   }};
