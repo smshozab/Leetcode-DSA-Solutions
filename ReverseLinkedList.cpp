1e7eeb6eb732cd698458a690a2c4ded0db51cee1
Shozab Mehdi <shozabmehdi89@gmail.com>
2024-02-28 08:34:47 +0500

    ReverseLinkedList.cpp Wed Feb 28 2024 08:34:47 GMT+0500 (Pakistan Standard Time)

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,21 @@
+/** * Definition for singly-linked list. * struct ListNode { *
+    int val; *
+    ListNode *next; *
+    ListNode() : val(0), next(nullptr) {} *
+    ListNode(int x) : val(x), next(nullptr) {} *
+    ListNode(int x, ListNode *next) : val(x), next(next) {} * }; */class Solution {public:
+   ListNode* reverseList(ListNode* head) {
+       ListNode* current = head;
+       ListNode *prev = NULL, *next = NULL;
+        while (current != NULL) {
+           // Store next
+           next = current->next;
+           // Reverse current node's pointer
+           current->next = prev;
+           // Move pointers one position ahead.
+           prev = current;
+           current = next;
+       }
+       head = prev;
+       return head;
+   }};
