a2416a17a0f3756a1dd2b179664d45b6e8ec8486
Shozab Mehdi <shozabmehdi89@gmail.com>
2024-05-14 17:59:53 +0500

    ValidAnagram.cpp Tue May 14 2024 17:59:51 GMT+0500 (Pakistan Standard Time)

--- ValidAnagram.cpp
+++ ValidAnagram.cpp
@@ -1,22 +1,13 @@
-class Solution {public:
+#include<unordered_map>class Solution {public:
    bool isAnagram(string s, string t) {
-       if (s.length() != t.length()) {
-           return false;
+       unordered_map<char,int> um;
+       for(int i=0;i<s.size();i++)
+           um[s[i]]++;
+       for(int i=0;i<t.size();i++)
+           um[t[i]]--;
+       for (auto it = um.begin(); it != um.end(); it++) {
+           if (it->second != 0)
+                return false;
        }
-       int charCount[256] = {0};
-       for (char c : s) {
-           charCount[c]++;
-       }
-       for (char c : t) {
-           if (charCount[c] == 0) {
-               return false;
-           }
-           charCount[c]--;
-       }
-               for (int i = 0; i < 256; i++) {
-           if (charCount[i] != 0) {
-               return false;
-           }
-       }
-               return true;
+       return true;
    }};
