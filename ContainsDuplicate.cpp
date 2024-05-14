518ea5c5e460552e7deb6472f0d9c849ed0129fd
Shozab Mehdi <shozabmehdi89@gmail.com>
2024-05-14 17:21:39 +0500

    ContainsDuplicate.cpp Tue May 14 2024 17:21:37 GMT+0500 (Pakistan Standard Time)

--- ContainsDuplicate.cpp
+++ ContainsDuplicate.cpp
@@ -1,10 +1,5 @@
 class Solution {public:
    bool containsDuplicate(vector<int>& nums) {
-       sort(nums.begin(), nums.end());
-       for (int i = 1; i < nums.size(); i++) {
-           if (nums[i] == nums[i - 1]) {
-               return true;
-           }
-       }
-       return false;
+       set<int> myset(nums.begin(), nums.end());
+       return myset.size() < nums.size();
    }};
